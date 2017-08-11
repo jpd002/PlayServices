74c60310962c6c671af5e39697c1e213dce0f40e
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2017-08-11 10:36:46 -0400

    Update database script.

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,12 @@
+CREATE TABLE `ps_games` (
+  `id` char(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8
+
 CREATE TABLE `ps_compatibility` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `gameId` varchar(20) NOT NULL,
+  `gameId` char(11) NOT NULL,
   `rating` int(11) NOT NULL,
   `deviceInfo` json NOT NULL,
   PRIMARY KEY (`id`)
