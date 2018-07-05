e6b70b00cabffff184beb988696704035b03c5c6
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2018-07-05 16:42:11 -0400

    Enable CORS.

--- server/Startup.cs
+++ server/Startup.cs
@@ -25,6 +25,12 @@ namespace PlayServices.Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("DefaultPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -39,6 +45,7 @@ namespace PlayServices.Server
             {
                 app.UseHsts();
             }
+            app.UseCors("DefaultPolicy");
             app.UseExceptionHandler(new ExceptionHandlerOptions
             {
                 ExceptionHandler = new JsonExceptionMiddleware().Invoke
