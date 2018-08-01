089ec1dd502d941df5fd1d1795334a0ffd9af601
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2018-08-01 09:08:30 -0400

    Add compatibility summary API.

---
+++ server/Controllers/CompatibilityController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Amazon.S3;
+
+namespace PlayServices.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompatibilityController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            var awsAccessKey = Environment.GetEnvironmentVariable("ps_builds_aws_access_key");
+            var awsSecretKey = Environment.GetEnvironmentVariable("ps_builds_aws_access_secret");
+            var creds = new Amazon.Runtime.BasicAWSCredentials(awsAccessKey, awsSecretKey);
+            var client = new AmazonS3Client(creds, Amazon.RegionEndpoint.USWest2);
+            var getRequest = new Amazon.S3.Model.GetObjectRequest
+            {
+                BucketName = "playcompatibility",
+                Key = "compat_summary.json"
+            };
+            string responseBody;
+            using (var response = await client.GetObjectAsync(getRequest))
+            {
+                using (StreamReader reader = new StreamReader(response.ResponseStream))
+                {
+                    responseBody = reader.ReadToEnd();
+                }
+            }
+            return Ok(responseBody);
+        }
+    }
+}
