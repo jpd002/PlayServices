079a389924fb7be8e51699b47cd2ab1096b695e4
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2021-04-23 20:30:54 -0400

    Add basic frontend to display game data.

---
+++ client/web_gamedata/src/App.tsx
@@ -0,0 +1,133 @@
+import React, { useState, useEffect } from 'react';
+import axios, { AxiosResponse, AxiosRequestConfig } from 'axios';
+import './App.css';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { Canvas } from '@react-three/fiber';
+import * as THREE from 'three';
+import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+function getAuthToken() : string | null {
+  const params = new URLSearchParams(window.location.search);
+  return params.get('token');
+}
+
+type GameData = {
+  gameId : string,
+  currentIndex: number,
+  lastModifiedDate: string,
+  iconUrl: string,
+};
+
+type GameDataListProps = {
+  authToken: string,
+};
+
+type GameIconProps = {
+  iconUrl: string,
+};
+
+function GameIcon(props : GameIconProps) {
+  const width = 200;
+  const height = 150;
+  const [icon, setIcon] = useState<GLTF | undefined>(undefined);
+
+  useEffect(() => {
+    const loader = new GLTFLoader();
+    loader.load(props.iconUrl,
+        (gltf) => { setIcon(gltf); },
+        (progress) => { },
+        (error) => { console.log(`Error: ${error}`); }
+    );
+  }, []);
+  
+  if(icon === undefined) {
+    return (
+      <Canvas style={ { width: width, height: height }} frameloop="demand">
+        <color attach="background" args={["lightgrey"]} />
+      </Canvas>
+    )
+  } else {
+    const bbox = new THREE.Box3().setFromObject(icon.scene);
+    const center = new THREE.Vector3();
+    bbox.getCenter(center);
+    return (
+      <Canvas style={ { width: width, height: height }} frameloop="demand">
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <color attach="background" args={["lightgrey"]} />
+        <primitive object={icon.scene} position={[-center.x, -center.y, -center.z]} />
+      </Canvas>
+    )
+  }
+}
+
+function GameDataList(props : GameDataListProps) {
+  const [data, setData] = useState<GameData[] | undefined>(undefined);
+  useEffect(() => {
+    const fetchData = async () => {
+      const requestConfig : AxiosRequestConfig = {
+        headers: {
+          Authorization: `Bearer ${props.authToken}`
+        }
+      };
+      try {
+        const response : AxiosResponse<GameData[]> = await axios('https://localhost:5001/api/users/me/gamedata', requestConfig);
+        setData(response.data);
+      } catch(err) {
+        console.log(err);
+      }
+    };
+    fetchData(); 
+  }, []);
+  if(data === undefined) {
+    return (
+      <div>Loading...</div>
+    );
+  } else {
+    return (
+      <List>
+        {
+          data.map(item => (
+            <ListItem key={item.gameId}>
+              <GameIcon iconUrl={item.iconUrl} />
+              <ListItemText primary={item.gameId} />
+              <ListItemText primary={item.lastModifiedDate} />
+            </ListItem>
+            )
+          )
+        }
+      </List>
+    );
+  }
+}
+
+function LoginButton() {
+  return (
+    <Button 
+      variant="contained"
+      color="primary"
+      onClick={function() { window.location.href = 'https://www.patreon.com/oauth2/authorize?response_type=code&client_id=ochJlBhW7r7bDjg3vcOeNu7k7rHWRKe26f89sPtH_Gku67wfr08VvT55peM_v6UU&redirect_url='; }}
+    >
+      Login
+    </Button>
+  )
+}
+
+function App() {
+  const authToken = getAuthToken();
+  //const authToken : string = 'blah';
+  if(authToken === null) {
+    return (
+      <LoginButton />
+    );
+  } else {
+    return (
+      <GameDataList authToken={ authToken } />
+    );
+  }
+}
+
+export default App;
